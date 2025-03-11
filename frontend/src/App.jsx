e23a2c6078d042bda1b11f8bbf8ecadcda07db02
yashash1111 <yashash.r.s70@kalvium.community>
2025-03-11 10:20:12 +0530

    cwcwc

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import TestimonialCard from "./components/TestimonialCard.jsx."
+import TestimonialCard from "./components/TestimonialCard.jsx"
 
 function App(props) {
 
